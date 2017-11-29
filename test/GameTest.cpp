3b02419a07cdfd976ae61642f6e72e4eafabf5c2
liyongshun <li.yongshun@zte.com.cn>
2017-11-29 21:13:03 +0800

    fix character format bug

--- test/GameTest.cpp
+++ test/GameTest.cpp
@@ -3,9 +3,9 @@
 
 struct GameTest : testing::Test
 {
-    GameTest() : game_sprint_1_2({{3, "ʯͷ"},{5, "����"},{7, "��"}})
-               , game_sprint_3({{5, "ʯͷ"},{7, "����"},{9, "��"}})
-               , game_sprint_4({{3, "�ϻ�"},{5, "����"},{7, "��"}})
+    GameTest() : game_sprint_1_2({{3, "石头"},{5, "剪刀"},{7, "布"}})
+               , game_sprint_3({{5, "石头"},{7, "剪刀"},{9, "布"}})
+               , game_sprint_4({{3, "老虎"},{5, "棒子"},{7, "鸡"}})
     {
     }
 
@@ -40,41 +40,41 @@ TEST_F(GameTest, should_count_off_given_special_num_3_5_7)
 {
     ASSERT_COUNTOFF_3_5_7(1,   "1");
     ASSERT_COUNTOFF_3_5_7(2,   "2");
-    ASSERT_COUNTOFF_3_5_7(3,   "ʯͷ");
-    ASSERT_COUNTOFF_3_5_7(5,   "����");
-    ASSERT_COUNTOFF_3_5_7(7,   "��");
-    ASSERT_COUNTOFF_3_5_7(15,  "ʯͷ����");
-    ASSERT_COUNTOFF_3_5_7(21,  "ʯͷ��");
-    ASSERT_COUNTOFF_3_5_7(105, "ʯͷ������");
+    ASSERT_COUNTOFF_3_5_7(3,   "石头");
+    ASSERT_COUNTOFF_3_5_7(5,   "剪刀");
+    ASSERT_COUNTOFF_3_5_7(7,   "布");
+    ASSERT_COUNTOFF_3_5_7(15,  "石头剪刀");
+    ASSERT_COUNTOFF_3_5_7(21,  "石头布");
+    ASSERT_COUNTOFF_3_5_7(105, "石头剪刀布");
 
-    ASSERT_COUNTOFF_3_5_7(13,   "ʯͷ");
-    ASSERT_COUNTOFF_3_5_7(35,   "ʯͷ");
+    ASSERT_COUNTOFF_3_5_7(13,   "石头");
+    ASSERT_COUNTOFF_3_5_7(35,   "石头");
 }
 
 TEST_F(GameTest, should_count_off_given_special_num_5_7_9)
 {
     ASSERT_COUNTOFF_5_7_9(1,   "1");
     ASSERT_COUNTOFF_5_7_9(2,   "2");
-    ASSERT_COUNTOFF_5_7_9(5,   "ʯͷ");
-    ASSERT_COUNTOFF_5_7_9(7,   "����");
-    ASSERT_COUNTOFF_5_7_9(9,   "��");
-    ASSERT_COUNTOFF_5_7_9(63,  "������");
+    ASSERT_COUNTOFF_5_7_9(5,   "石头");
+    ASSERT_COUNTOFF_5_7_9(7,   "剪刀");
+    ASSERT_COUNTOFF_5_7_9(9,   "布");
+    ASSERT_COUNTOFF_5_7_9(63,  "剪刀布");
 
-    ASSERT_COUNTOFF_5_7_9(15,  "ʯͷ");
-    ASSERT_COUNTOFF_5_7_9(35,  "ʯͷ");
+    ASSERT_COUNTOFF_5_7_9(15,  "石头");
+    ASSERT_COUNTOFF_5_7_9(35,  "石头");
 }
 
 TEST_F(GameTest, should_count_off_given_special_num_3_5_7_countof_other)
 {
     ASSERT_COUNTOFF_3_5_7_EX(1,   "1");
     ASSERT_COUNTOFF_3_5_7_EX(2,   "2");
-    ASSERT_COUNTOFF_3_5_7_EX(3,   "�ϻ�");
-    ASSERT_COUNTOFF_3_5_7_EX(5,   "����");
-    ASSERT_COUNTOFF_3_5_7_EX(7,   "��");
-    ASSERT_COUNTOFF_3_5_7_EX(15,  "�ϻ�����");
-    ASSERT_COUNTOFF_3_5_7_EX(21,  "�ϻ���");
-    ASSERT_COUNTOFF_3_5_7_EX(105, "�ϻ����Ӽ�");
+    ASSERT_COUNTOFF_3_5_7_EX(3,   "老虎");
+    ASSERT_COUNTOFF_3_5_7_EX(5,   "棒子");
+    ASSERT_COUNTOFF_3_5_7_EX(7,   "鸡");
+    ASSERT_COUNTOFF_3_5_7_EX(15,  "老虎棒子");
+    ASSERT_COUNTOFF_3_5_7_EX(21,  "老虎鸡");
+    ASSERT_COUNTOFF_3_5_7_EX(105, "老虎棒子鸡");
 
-    ASSERT_COUNTOFF_3_5_7_EX(13,  "�ϻ�");
-    ASSERT_COUNTOFF_3_5_7_EX(35,  "�ϻ�");
+    ASSERT_COUNTOFF_3_5_7_EX(13,  "老虎");
+    ASSERT_COUNTOFF_3_5_7_EX(35,  "老虎");
 }
